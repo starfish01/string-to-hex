b119eefeb30e9a9f567a023388a500c894ac9487
Digistorm <digistorm@Digistorms-MacBook-Pro.local>
2021-03-02 21:05:55 +1000

    adding port

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.get('/', function (req, res) {
 
 app.get('/:convertStrings',(req, res) =>{
     const textToConvert = req.params.convertStrings;
-    
     res.send(toHex(textToConvert.toUpperCase()));
 
 });
@@ -25,6 +24,6 @@ function toHex(string){
     return msg;
 }
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
